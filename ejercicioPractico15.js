039b788be0c6885381117216e2b78d75d98f6b02
Christian Jesús Mino Egusquiza <christianjme@gmail.com>
2022-11-29 16:24:40 -0500

    ejercicio15 - Simulación de sorteo digital

---
+++ ejercicioPractico15.js
@@ -0,0 +1,24 @@
+function draw(btn, selector){
+
+  const getWinner = selector => {
+    const $players = document.querySelectorAll(selector),
+          random = Math.floor(Math.random()*$players.length),
+          winner = $players[random]
+
+    return `El ganador es ${winner.textContent}`
+  }
+
+
+  document.addEventListener('click', e=>{
+    if (e.target.matches(btn)) {
+      let result = getWinner(selector)
+
+      alert(result)
+      console.log(result)
+    }
+  })
+}
+
+document.addEventListener('DOMContentLoaded', ()=>{
+  draw('#btn-winner','.player')
+})
